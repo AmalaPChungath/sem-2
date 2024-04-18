8c72305705dec236cbaa1886d55751bcae1bc8cb
Amala P Chungath <147935213+AmalaPChungath@users.noreply.github.com>
2024-04-18 12:04:29 +0530

    Update aob.java

--- java/programs/aob.java
+++ java/programs/aob.java
@@ -15,25 +15,14 @@ class employee
 		System.out.println("eno="+eno);
 		System.out.println("ename="+ename);
 		System.out.println("esalary="+esalary);
-	}
-	 void search(int eno,int n)
-		{
-			for(int i=0;i<n;i++)
-			{
-				if(this.eno==eno)
-				{
-					this.display();	
-				}
-				this=this+sizeOf(this);
-			}
-		}	
+	}	
 }
 class aob
 {
 	public static void main(String args[])
 	{
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter no of employees to be enetered: ");
+		System.out.println("Enter no of employees to be entered: ");
 		int n=s.nextInt();
 		employee arr[]=new employee[n];
 		for(int i=0;i<n;i++)
@@ -44,20 +33,15 @@ class aob
 		{
 			arr[i].display();
 		}
-		
-		/*void search()
-		{
-			for(int i=0;i<n;i++)
-			{
-				if(arr[i].eno=eno)
-				{
-					arr[i].display();	
-				}
-			}
-		}	*/
 		System.out.println("Enter eno to be searched");
 		int e=s.nextInt();
-		arr.search(e,n);
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i].eno==e)
+			{
+				arr[i].display();	
+			}
+		}
 	}
 	
 }
