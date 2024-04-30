e98907457665a3394ad5ba7837bf551e89f5b147
Amala P Chungath <147935213+AmalaPChungath@users.noreply.github.com>
2024-04-30 10:57:41 +0530

    Update addnum.sql

--- dbms/addnum.sql
+++ dbms/addnum.sql
@@ -10,15 +10,11 @@ end addnum;
 
 /*
 
-Set Serveroutput ON
-create or replace function addnum(v1 in number,v2 in number)
-return number is
-r number;
-begin
-  r:=v1+v2;
-  return r;
-end addnum;
-/
+SQL> select addnum(5,6) "sum = " from dual;
+
+    sum =
+----------
+	11
 
 
 */
